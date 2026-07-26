89415d29e6f3fd3acb7a07bc838df360fbf25b0c
mingwan0309 <rlaalsrhks93@gmail.com>
2026-07-26 19:22:26 +0900

    학부모 로그인 재발급 버튼 문구 개선 - 문제 있는 것처럼 보이지 않게 수정, 이미 계정 있는 경우 안심 메시지로 변경

--- firebase-api.js
+++ firebase-api.js
@@ -225,7 +225,7 @@ api.createParentAccount = function(db, p){
       .catch(function(err){
         return secondary.auth().signOut().catch(function(){}).then(function(){
           if (err.code === 'auth/email-already-in-use') {
-            return { success:false, code:err.code, msg:'이미 사용 중인 번호예요. Firebase 콘솔 Authentication에서 '+parentPhone+'@mkmath.local 계정을 먼저 삭제해주세요 (예전 삭제된 학생의 고아 계정일 수 있어요).' };
+            return { success:false, code:err.code, msg:'이 학부모 번호는 이미 로그인 계정이 있어요(정상). 그래도 로그인이 안 되면 비밀번호(기본 123456) 문제일 수 있어요. 정말 안 되면 Firebase 콘솔 Authentication에서 '+parentPhone+'@mkmath.local 계정을 삭제 후 다시 시도해보세요.' };
           }
           if (err.code === 'auth/too-many-requests') {
             return { success:false, code:err.code, msg:'요청이 너무 잦습니다. 잠시 후 다시 시도해주세요.' };
